a6956c332f2a9baff430a8557ab68afd33bab4b6
manuligit <riina.vaisanen@gmail.com>
2017-03-31 12:22:07 +0300

    asdf

--- app/src/test/java/test/com/jenkinstest/ExampleUnitTest.java
+++ app/src/test/java/test/com/jenkinstest/ExampleUnitTest.java
@@ -11,6 +11,7 @@ import static org.junit.Assert.*;
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * asdf
  */
 public class ExampleUnitTest {
     @Test
