1738c28fd826e2c04b1e2b2e3e3d69f73471013e
manuligit <riina.vaisanen@gmail.com>
2017-04-03 10:58:25 +0300

    Failing tests

--- app/src/test/java/test/com/jenkinstest/ExampleUnitTest.java
+++ app/src/test/java/test/com/jenkinstest/ExampleUnitTest.java
@@ -11,11 +11,11 @@ import static org.junit.Assert.*;
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- * asdf
+ * /usr/local/opt/android-sdk
  */
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+        assertEquals(4, 2 + 3);
     }
 }
