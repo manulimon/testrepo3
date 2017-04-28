c8fe866d1d652d6d437dcd7c077df31086ec311e
manulimon <aoionaga@gmail.com>
2017-04-28 10:16:45 +0300

    Correcting typos

--- app/src/test/java/test/com/jenkinstest/ExampleUnitTest.java
+++ app/src/test/java/test/com/jenkinstest/ExampleUnitTest.java
@@ -15,7 +15,7 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isFalse() throws Exception {
+    public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
     /*
